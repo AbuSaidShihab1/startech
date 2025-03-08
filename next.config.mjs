a2bac27446fc2193d09835304f78cfdc54062d0c
shihab <shihabmoni15@gmail.com>
2025-03-08 16:38:17 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables standalone mode for deployment
     images: {
       unoptimized: true, // Fixes issues with image optimization on cPanel
     },
